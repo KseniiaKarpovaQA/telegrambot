395b20f04eb8fb50c74854f969ed4b4b2427990f
Kseniia Karpova <k.karpova.spb@gmail.com>
2025-05-06 12:55:41 +0100

    Add files via upload

---
+++ TestCardMasterBot/README.md
@@ -0,0 +1,38 @@
+<h2>💳 Card Generator Telegram Bot</h2>
+
+## 🚀 Why This Bot Exists
+During software testing, especially for payment systems, QA engineers often need valid-looking card numbers that pass standard validation checks but don’t connect to real accounts.
+This bot simplifies that process by generating numbers on demand — all from your Telegram.
+
+## ✔️ Key Capabilities
+Provides instantly generated card numbers
+
+Uses the Luhn algorithm to ensure structural validity
+
+Supports multiple formats:
+
+VISA
+
+Mastercard
+
+American Express
+
+JCB
+
+No setup beyond clicking a few buttons in chat
+
+## 📸 Screenshots
+
+
+![image]()
+
+
+![image]()
+
+
+## 💻 Built With
+
+* Python
+* `telebot` library
+* `faker` library
+
---
+++ TestCardMasterBot/main.py
@@ -0,0 +1,60 @@
+
+from telebot import TeleBot, types
+from faker import Faker
+
+
+bot = TeleBot(token='7580395787:AAHo2Y5OdU5XD6HyG0tTDPZWST7n69F-9sE', parse_mode='html')
+faker = Faker()
+
+card_type_keybaord = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+card_type_keybaord.row(
+    types.KeyboardButton(text='VISA'),
+    types.KeyboardButton(text='Mastercard'),
+)
+
+card_type_keybaord.row(
+    types.KeyboardButton(text='JCB'),
+    types.KeyboardButton(text='American Express'),
+)
+
+
+@bot.message_handler(commands=['start'])
+def start_command_handler(message: types.Message):
+    bot.send_message(
+        chat_id=message.chat.id,
+        text='👋 Hi there! I can help you generate a test credit card number. Just pick a card type and I\'ll show you the number! 💳\nChoose a card type:', 
+        reply_markup=card_type_keybaord,
+    )
+
+@bot.message_handler()
+def message_handler(message: types.Message):
+
+    if message.text == 'VISA':
+        card_type = 'visa'
+    elif message.text == 'Mastercard':
+        card_type = 'mastercard'
+    elif message.text == 'JCB':
+        card_type = 'jcb'
+    elif message.text == 'American Express':
+        card_type = 'amex'   
+    else:
+        bot.send_message(
+            chat_id=message.chat.id,
+            text="Sorry! I did't anything",
+        )
+        return
+
+    card_number = faker.credit_card_number(card_type)
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f'Card {card_type}:\n<code>{card_number}</code>'
+    )
+
+
+def main():
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    main()
